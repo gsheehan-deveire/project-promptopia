f301254b6b2937bce0274c5210f9697cccf30227
gsheehan-deveire <georgina@deveire.com>
2024-07-18 10:35:48 +0100

    Updated paths to files

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
-import '@styles/globals.css';
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import Nav from '../components/Nav';
+import Provider from '../components/Provider';
+import '../styles/globals.css'
 
 export const metadata = {
     title: "Promptopia",
